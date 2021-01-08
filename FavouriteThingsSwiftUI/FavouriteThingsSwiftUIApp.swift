2037b1c07ce5c3e88fc3396f90d7899a87788f63
Russell Gordon <github@russellgordon.ca>
2021-01-08 15:30:38 -0500

    Initial Commit

---
+++ FavouriteThingsSwiftUI/FavouriteThingsSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  FavouriteThingsSwiftUIApp.swift
+//  FavouriteThingsSwiftUI
+//
+//  Created by Russell Gordon on 2021-01-08.
+//
+
+import SwiftUI
+
+@main
+struct FavouriteThingsSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
